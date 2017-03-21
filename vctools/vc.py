b627775619e9220b4acc73f8662df57eaeb11f2d
Ivan Ryabchenko <rikdev@yandex.ru>
2017-03-21 18:17:21 +0700

    Added Visual Studio 2017 support

--- vctools/vc.py
+++ vctools/vc.py
@@ -3,6 +3,7 @@
 
 import os
 import subprocess
+import sys
 
 
 class VCTools:
@@ -11,7 +12,7 @@ class VCTools:
     This class look for installed Visual Studio toolset and downloads
     environment for it."""
 
-    VERSIONS = {'2012': 11, '2013': 12, '2015': 14, '15': 15}
+    VERSIONS = {'2012': 11, '2013': 12, '2015': 14, '2017': 15}
     COMMANDS = [
         'check_call',
         'devenv',
@@ -39,7 +40,7 @@ class VCTools:
         """
         Args:
             version_name (str or None): Visual Studio version name ('2012',
-                '2013', '2015', etc.). If None, it uses the most recent
+                '2013', '2015', '2017', etc.). If None, it uses the most recent
                 installed Visual Studio toolset (supported from this class, see
                 VERSIONS)
             platform (str): Host_target platform ('x86', 'amd64', 'x86_amd64',
@@ -76,15 +77,24 @@ class VCTools:
                     continue
                 break
         if self.environ is None:
-            raise self.NotInstalledError('Visual Studio is not installed')
+            raise self.NotInstalledError('Visual Studio is not installed '
+                                         'for target platform')
 
     def get_vc_install_dir(self):
         '''Returns the Visual C install directory.'''
         return self.environ['VCINSTALLDIR']
 
+    def get_vs_install_dir(self):
+        '''Returns the Visual Studio install directory.'''
+        vs_install_dir = self.environ.get('VSINSTALLDIR')
+        if not vs_install_dir:
+            version = self.VERSIONS[self.version_name]
+            vs_install_dir = self.__get_vs_install_dir(version)
+        return vs_install_dir
+
     def get_target_platform(self):
         '''Returns the target platform name.'''
-        return self.environ.get('PLATFORM', 'X86')
+        return self.environ.get('PLATFORM', 'X86').upper()
 
     def check_call(self, *nargs, **kwargs):
         '''Run command with arguments (equivalent subprocess.check_call) in
@@ -95,24 +105,13 @@ class VCTools:
         kwargs['shell'] = True
         return subprocess.check_call(*nargs, **kwargs)
 
-    def __get_vsvars(self, toolset_version, platform):
-        try:
-            vs_tools_env_name = 'VS{}0COMNTOOLS'.format(toolset_version)
-            vs_tools_path = os.environ.get(vs_tools_env_name)
-            if vs_tools_path is None:
-                raise ValueError('The environment variable {} is not found'
-                                 .format(vs_tools_env_name))
-            vcvarsall_path = os.path.normpath(os.path.join(vs_tools_path,
-                                                           '..',
-                                                           '..',
-                                                           'VC',
-                                                           'vcvarsall.bat'))
-            if not os.path.exists(vcvarsall_path):
-                raise ValueError('Invalid path "{}" to script vcvarsall.bat'
-                                 .format(vcvarsall_path))
-        except ValueError as e:
-            raise self.NotInstalledError('Visual Studio {} is not installed: {}'
-                                         .format(toolset_version, e.args))
+    def __get_vsvars(self, version, platform):
+        vs_path = self.__get_vs_install_dir(version)
+        if version < 15:
+            vcvarsall_path = os.path.join(vs_path, 'VC', 'vcvarsall.bat')
+        else:
+            vcvarsall_path = os.path.join(vs_path, 'VC', 'Auxiliary', 'Build',
+                                          'vcvarsall.bat')
 
         cmd = [vcvarsall_path, platform]
         # vcvarsall.bat always returns 0, but in case of error it out put error
@@ -120,9 +119,10 @@ class VCTools:
         popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE)
         stdout, stderr = popen.communicate()
-        if popen.returncode != 0 or stdout or stderr:
-            raise self.PlatformError('\n'.join([stdout.decode('cp866'),
-                                                stderr.decode('cp866')]))
+        stdout_str = stdout.decode()
+        if popen.returncode != 0 or 'ERROR' in stdout_str.upper() or stderr:
+            raise self.PlatformError('\n'.join([stdout_str,
+                                                stderr.decode()]))
 
         return _get_environ(cmd)
 
@@ -136,6 +136,44 @@ class VCTools:
         if cmd_args is not None:
             command += cmd_args if isinstance(cmd_args, list) else [cmd_args]
         return self.check_call(command, *nargs, **kwargs)
+
+
+    def __get_vs_install_dir(self, version):
+        def find_vs_path_by_environ(version):
+            vs_tools_env_name = 'VS{}0COMNTOOLS'.format(version)
+            try:
+                vs_tools_path = os.environ[vs_tools_env_name]
+                return os.path.normpath(os.path.join(vs_tools_path, '..', '..'))
+            except KeyError:
+                pass
+            return None
+
+        def find_vs_path_by_registry(version):
+            if sys.version_info >= (3, 0):
+                import winreg as reg
+                exception = FileNotFoundError
+            else:
+                import _winreg as reg
+                exception = WindowsError
+
+            sub_key = r'SOFTWARE\Microsoft\VisualStudio\SxS\VS7'
+            try:
+                with reg.OpenKey(reg.HKEY_LOCAL_MACHINE, sub_key, 0,
+                                 reg.KEY_QUERY_VALUE|reg.KEY_WOW64_32KEY) as key:
+                    value_name = '{}.0'.format(version)
+                    vs_path = reg.QueryValueEx(key, value_name)[0]
+                    return os.path.normpath(vs_path)
+            except exception:
+                pass
+            return None
+
+        vs_install_dir = find_vs_path_by_environ(version)
+        if not vs_install_dir:
+            vs_install_dir = find_vs_path_by_registry(version)
+        if not vs_install_dir:
+            raise self.NotInstalledError("Can't find VisualStudio/{}."
+                                         .format(version))
+        return vs_install_dir
 
 
 def _get_environ(args=None):
